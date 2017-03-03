96f61c9dc10878975ef49735df04b4fe7ed0cc55
Stacy <Stacy@DESKTOP-50AU5UD>
2017-03-02 21:27:02 -0800

    Final

---
+++ src/cpts132/sudoku/SudokuBoard.java
@@ -0,0 +1,20 @@
+package cpts132.sudoku;
+
+public class SudokuBoard extends javax.swing.JComponent {
+    
+    public SudokuBoard(SudokuBase b) {
+    }
+    
+    SudokuBase getBase() {
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        javax.swing.JFrame win = new javax.swing.JFrame("Test 2x3");
+        win.setDefaultCloseOperation(javax.swing.JFrame.DISPOSE_ON_CLOSE);
+        win.add(new SudokuBoard(new SudokuStub(2, 3)));
+        win.pack();
+        win.setVisible(true);
+    }
+    
+}

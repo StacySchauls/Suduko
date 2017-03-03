bcfb22264ca3e1fea8ca26418f3dacbf226adf56
Stacy <Stacy@DESKTOP-50AU5UD>
2017-03-02 21:16:05 -0800

    Final

---
+++ src/suduko/Suduko.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suduko;
+
+/**
+ *
+ * @author Stacy
+ */
+public class Suduko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Just seeing if this works
+        // TODO code application logic here
+    }
+    
+}

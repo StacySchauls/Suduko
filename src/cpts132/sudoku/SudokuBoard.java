a25a3c44779374119a5b5577f4a435693e885ff2
Stacy <Stacy@DESKTOP-50AU5UD>
2017-03-08 20:29:09 -0800

    Final

--- src/cpts132/sudoku/SudokuBoard.java
+++ src/cpts132/sudoku/SudokuBoard.java
@@ -1,23 +1,27 @@
 package cpts132.sudoku;
 
-import java.awt.BorderLayout;
+
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.BorderFactory;
 
 
 public class SudokuBoard extends javax.swing.JComponent {
-  
-        
+  //the dimensions of the board
+    private int rows;
+    private int cols;
+    private int size;
     
-    
+    /**
+     * constructor for the class. sets up the rows, columns and size
+     * @param b the sudoku base
+     */
     public SudokuBoard(SudokuBase b) {
-        setPreferredSize(new java.awt.Dimension(b.getSize()*50, b.getSize()*50));
-       
+      setPreferredSize(new java.awt.Dimension(b.getSize()*50, b.getSize()*50));
+      this.setBorder(BorderFactory.createLineBorder(Color.black));
+      rows = b.getRows();
+      cols = b.getColumns();
+      size = b.getSize();
+
     }
     
     SudokuBase getBase() {
@@ -25,59 +29,35 @@ public class SudokuBoard extends javax.swing.JComponent {
     }
     
     public static void main(String[] args) {
-        JPanel controls;
-        SudokuBase base;
-        drawBoard board;
-        SelectedCell selected;
-        JLabel output;
-        JLabel error;
-        JButton query;
-        JButton set;
-        JTextField txtRow;
-        JTextField txtCol;
-        JFrame win;
         
+ 
+        new SudokuBoardTest(2,3);
         
-       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        win = new JFrame("Title");
-        win.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        JPanel pnl = new JPanel();
-        pnl.setBackground(Color.cyan);
-        
-        board = new drawBoard(3,2);
-        
-        pnl.add(board);
-        
-        win.add(pnl);
-        //win.add(controls, java.awt.BorderLayout.SOUTH);
-       // controls.add(pnl = new JPanel());
-        SelectCell cell = new SelectCell();
-        pnl.addMouseListener(cell);
-        pnl.add(new JButton("Query"));
-        board.add(new JButton("Button"));
-        
-        int column = cell.getSelectedColumn();
-        int selectRow = cell.getSelectedRow();
-        HighlightCell high = new HighlightCell(column, selectRow);
-       
-       board.add(high); 
-       
-        
-        
-        
-        win.pack();
-        win.setVisible(true);
-        
+   
     }
+ 
+   
+    @Override
+    /**
+     * Paint component of JComponent, draws the Sudoku board
+     * @param g the graphics object
+     */
+   public void paintComponent(java.awt.Graphics g){
+       super.paintComponent(g);
+       System.out.println("Cols: " + cols +" Rows: "+rows +" Size: "+size);
+       g.setColor(Color.black);
+       for(int k= 0; k<size; k++){
+            for(int i = 0; i<size; i++){
+                    g.setColor(Color.white);
+                    g.fillRect(i*50, k*50, 49, 49);
+                    g.setColor(Color.black);
+                    g.drawRect(i*50, k*50, 49, 49);
+
+            }
+       }
+       
+       
+     }
     
 }
    
